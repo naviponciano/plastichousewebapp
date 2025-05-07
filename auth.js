2618abe420b2e22466f2f897da0d886b5073a0a7
Navieska Ponciano <144177184+naviponciano@users.noreply.github.com>
2025-05-06 23:00:48 -0600

    Autenticacion

--- auth.js
+++ auth.js
@@ -1,8 +1,9 @@
 function verifyUserSession(){
     const token = localStorage.getItem('authtoken') ?? '';
-    if (token){
+    if (!token) {
         const url = `${window.location.origin}/auth/login.html`;
         window.location.href = url;
     }
 }
+
 verifyUserSession();
