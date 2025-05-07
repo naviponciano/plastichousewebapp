d584204d6d30ae79436a1531342000458ab38d76
Navieska Ponciano <144177184+naviponciano@users.noreply.github.com>
2025-05-06 22:45:03 -0600

    Admin panel

---
+++ auth.js
@@ -0,0 +1,8 @@
+function verifyUserSession(){
+    const token = localStorage.getItem('authtoken') ?? '';
+    if (token){
+        const url = `${window.location.origin}/auth/login.html`;
+        window.location.href = url;
+    }
+}
+verifyUserSession();
