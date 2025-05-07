2276b9f2f8b730a53164ac6277ec5302529cd276
jaharyyy <jaharygutierrez6@gmail.com>
2025-05-07 00:41:15 -0600

    Mostrar user y password

--- Auth/login.js
+++ Auth/login.js
@@ -1,3 +1,8 @@
 function handleLoginSubmit(event) {
+    event.preventDefault ();
 console.log(event);
+const Form = event.target ;
+const FormData = new FormData (Form);
+const UserName = FormData.get ('username');
+const Password =FormData.get ('password');
 }
