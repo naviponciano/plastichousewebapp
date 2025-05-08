232df8a2f45728b0ab31147acea84c2cb031d828
jaharyyy <jaharygutierrez6@gmail.com>
2025-05-07 18:44:04 -0600

    Form Login

---
+++ Auth/Login/login.js
@@ -0,0 +1,6 @@
+const btnIr = document.getElementById("Btn");
+const Container = document.querySelector(".Login-container");
+
+btn.addEventListener("Ir",()=>(
+    Login.classList.toggle("Toggle")
+));
